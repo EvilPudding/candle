4d6516456f635767dd28648d038dd49ac1a5db30
EvilPudding <jota.rod@hotmail.com>
2018-10-29 03:02:53 +0000

    Added alpha check to shadows

--- shaders/depth.glsl
+++ shaders/depth.glsl
@@ -6,9 +6,11 @@ layout (location = 0) out vec4 FragColor;
 
 void main()
 {
+	vec4 dif  = resolveProperty(mat(albedo), texcoord);
+	if(dif.a < 0.7f) discard;
+
 	float dist = length(vertex_position);
     FragColor = vec4(0, 0, 0, dist);
-
 }  
 
 // vim: set ft=c:
