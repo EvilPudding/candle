a5680c769df9a1bb718a437ec13c37c1604d58f4
sirevilpudding <jota.rod@hotmail.com>
2019-06-02 15:34:33 +0100

    Added missing strtok_r on mingw

--- vil/vil.c
+++ vil/vil.c
@@ -104,6 +104,21 @@ static vicall_t *vifunc_get_call(vifunc_t *type, const slot_t slot,
 	return NULL;
 }
 
+#ifdef WIN32
+char* strtok_r(char *str, const char *delim, char **nextp)
+{
+    char *ret;
+    if (str == NULL) str = *nextp;
+    str += strspn(str, delim);
+    if (*str == '\0') return NULL;
+    ret = str;
+    str += strcspn(str, delim);
+    if (*str) *str++ = '\0';
+    *nextp = str;
+    return ret;
+}
+#endif
+
 slot_t vifunc_slot_from_name(vifunc_t *func, char *buffer,
                              const char *separator)
 {
